fedecbbe18fd766dd9a872c27cd968578abd92fb
mdaniel3 <mdaniel3@mail.uni-paderborn.de>
2025-02-10 15:41:32 +0100

    changes env from dynamic to static

--- web/src/lib/pocketbase.ts
+++ web/src/lib/pocketbase.ts
@@ -1,8 +1,8 @@
-import { env } from '$env/dynamic/public';
+import { PUBLIC_POCKETBASE_URL } from '$env/static/public';
 import PocketBase from 'pocketbase';
 
 export function createInstance() {
-	return new PocketBase(env.PUBLIC_POCKETBASE_URL);
+	return new PocketBase(PUBLIC_POCKETBASE_URL);
 }
 
 export const pb = createInstance();
