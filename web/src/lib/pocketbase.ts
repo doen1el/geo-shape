b31ceee198a9510eb4e01b21ba0027347afd25f0
mdaniel3 <mdaniel3@mail.uni-paderborn.de>
2025-02-11 20:33:43 +0100

    working on dockerization

--- web/src/lib/pocketbase.ts
+++ web/src/lib/pocketbase.ts
@@ -2,6 +2,7 @@ import { PUBLIC_POCKETBASE_URL } from '$env/static/public';
 import PocketBase from 'pocketbase';
 
 export function createInstance() {
+	console.log('PUBLIC_POCKETBASE_URL:', PUBLIC_POCKETBASE_URL);
 	return new PocketBase(PUBLIC_POCKETBASE_URL);
 }
 
