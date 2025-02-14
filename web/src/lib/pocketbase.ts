d46e2cdada12b3e99d810e0267dac5802883532a
mdaniel3 <mdaniel3@mail.uni-paderborn.de>
2025-02-14 09:54:45 +0100

    changed /api to /pb

--- web/src/lib/pocketbase.ts
+++ web/src/lib/pocketbase.ts
@@ -1,8 +1,8 @@
 import PocketBase from 'pocketbase';
 
 export function createInstance() {
-	console.log('PUBLIC_POCKETBASE_URL:', 'https://geoshape.danielmuenstermann.de/api');
-	return new PocketBase('https://geoshape.danielmuenstermann.de/api');
+	console.log('PUBLIC_POCKETBASE_URL:', 'https://geoshape.danielmuenstermann.de/pb');
+	return new PocketBase('https://geoshape.danielmuenstermann.de/pb');
 }
 
 export const pb = createInstance();
